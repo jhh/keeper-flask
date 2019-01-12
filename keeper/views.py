43f9f40d93a1fd2ee0e77c17a307509ecbea1450
Jeff Hutchison <jeff@jeffhutchison.com>
2019-01-12 14:50:37 -0500

    Convert to pipenv, add pages

--- keeper/views.py
+++ keeper/views.py
@@ -4,11 +4,35 @@ from keeper import app
 from keeper.db import get_db_cursor
 import keeper.db
 
-
 @app.route('/')
 def index():
     app.logger.warning('sample message')
     with get_db_cursor(commit=True) as cursor:
         cursor.execute("SELECT count(*) FROM ACTIVITY")
         app.logger.warn(" activity count = {}".format(cursor.fetchone()[0]))
-    return render_template('index.html')
+    return render_template('index.html.j2')
+
+@app.route('/activities/')
+def activities():
+    return render_template('activities.html.j2')
+
+ACTIONS_SQL = """
+SELECT id, name, timestamp
+FROM action
+ORDER BY timestamp DESC
+LIMIT 20
+"""
+
+@app.route('/actions/')
+def actions():
+    with get_db_cursor(commit=True) as cursor:
+        cursor.execute(ACTIONS_SQL)
+        return render_template('actions.html.j2', actions=cursor.fetchall())
+
+
+@app.template_filter('formatdatetime')
+def format_datetime(value, format="%d %b %Y %I:%M %p"):
+    """Format a date time to (Default): d Mon YYYY HH:MM P"""
+    if value is None:
+        return ""
+    return value.strftime(format)
