e853398aa76de19e2d70bcfaaa067588ab0ef09e
Jeff Hutchison <jeff@jeffhutchison.com>
2019-01-21 09:17:19 -0500

    Redo plots

--- keeper/plots.py
+++ keeper/plots.py
@@ -7,6 +7,8 @@ from keeper.db import get_db_connection
 
 matplotlib.use("Agg")
 
+grid = False
+
 
 def motion_profile_plotdata(action_id):
     trace_df = None
@@ -22,13 +24,12 @@ def motion_profile_plotdata(action_id):
     vel_df.profile_vel = vel_df.profile_vel / 10.0
     vel_df["ticks_error"] = vel_df.actual_ticks - vel_df.profile_ticks
 
-    fig, (vel_ax, ticks_ax, gyro_ax) = plt.subplots(
-        nrows=3, ncols=1, sharex=True, figsize=(10, 15)
+    fig, (vel_ax, ticks_ax) = plt.subplots(
+        nrows=2, ncols=1, sharex=True, figsize=(12, 12)
     )
-    vel_ax.set(title="Velocity", ylabel="ticks/100ms")
-    ticks_ax.set(title="Distance Error", ylabel="ticks")
-    gyro_ax.set(title="Gyro Angle", ylabel="degrees")
-    vel_df[["profile_vel", "setpoint_vel", "actual_vel"]].plot(ax=vel_ax, grid=False)
+    vel_ax.set(title="Velocity and Current", ylabel="ticks/100ms")
+    ticks_ax.set(title="Gyro and Distance Error", ylabel="ticks")
+    vel_df[["profile_vel", "setpoint_vel", "actual_vel"]].plot(ax=vel_ax, grid=grid)
 
     if 'drive_current' in vel_df.columns:
         i_ax = vel_ax.twinx()
@@ -36,8 +37,11 @@ def motion_profile_plotdata(action_id):
         i_ax.set_ylabel("current", color="r")
         i_ax.tick_params("y", colors="r")
 
-    vel_df.ticks_error.plot(ax=ticks_ax, grid=False, legend=False)
-    vel_df.gyro_angle.plot(ax=gyro_ax, grid=False, legend=False)
+    gyro_ax = ticks_ax.twinx()
+    vel_df.ticks_error.plot(ax=ticks_ax, grid=grid, legend=False)
+    vel_df.gyro_angle.plot(ax=gyro_ax, grid=grid, color="r")
+    gyro_ax.set_ylabel("gyro angle", color="r")
+    gyro_ax.tick_params("y", colors="r")
 
     bytes_image = io.BytesIO()
     fig.savefig(bytes_image, format="png")
